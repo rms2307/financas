f01df5d6dd964351ace0ca7039eacd2e51e63594
Robson Silva <robson.silva@tripletech.com.br>
2021-03-06 20:55:54 -0300

    componente BotaoAdd

---
+++ src/components/BotaoAdd.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { TouchableOpacity, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import colors from '../common/colors'
+
+const BotaoAdd = () => {
+    return (
+        <TouchableOpacity style={styles.addButton}
+            activeOpacity={0.7} >
+            <Icon name='plus' size={20}
+                color={'#fff'} />
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'space-between',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
+    },
+    addButton: {
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+        width: 50,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.primary.dark
+    }
+})
+
+export default BotaoAdd
